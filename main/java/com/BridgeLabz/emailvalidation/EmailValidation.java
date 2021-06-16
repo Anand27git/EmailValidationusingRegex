e47cdf59fe5ef98b6ce2905a02895ed6efc24eda
Anand27git <anandkumar256k@gmail.com>
2021-06-16 22:41:11 +0530

    validating second mandatory part @symbol and Bridgelabz

--- main/java/com/BridgeLabz/emailvalidation/EmailValidation.java
+++ main/java/com/BridgeLabz/emailvalidation/EmailValidation.java
@@ -5,14 +5,15 @@ import java.util.regex.Pattern;
 /********************
  * 
  * @author ANAND 
- * purpose: Validation of Email-ID start with abc
- *
+ * purpose: Validation of Email-ID
+ * UC-1 Validation of Email-ID start with abc
+ * UC-2 to Ensure the @ and mandatory 2nd part BridgeLabz
  ********************/
 public class EmailValidation {
 
-	// UC-1 method to validate mandatory part start with abc
+	// UC-2 to Ensure the @ and mandatory 2nd part BridgeLabz
 	public boolean validemail(String email) {
-		return Pattern.matches("([a-zA-Z0-9.]+)", email);
+		return Pattern.matches("^[a-zA-Z0-9.@a-zA-Z0-9.]+$", email);
 	}
 
 }
--- test/java/com/BridgeLabz/emailvalidation/EmailValidationtest.java
+++ test/java/com/BridgeLabz/emailvalidation/EmailValidationtest.java
@@ -8,7 +8,8 @@ import org.junit.Test;
  * 
  * @author ANAND 
  * UC-1 Test case to validate mandatory part start with abc
- *
+ * UC-2 Test case to Ensure the @ and mandatory 2nd part BridgeLabz
+ * 
  *******************/
 public class EmailValidationtest {
 
@@ -16,8 +17,18 @@ public class EmailValidationtest {
 	// UC-1 Test case to validate mandatory part start with abc
 	public void EmailValidationItShouldReturnValidOrNot() {
 		EmailValidation email = new EmailValidation();
-		assertTrue(email.validemail("abc.123")); // validated
+		assertTrue(email.validemail("abc.xyz")); // validated
 		assertTrue(email.validemail("Anand.256"));
 		// test case passed
 	}
+
+	@Test
+	// UC-2 Test case to Ensure the @ and mandatory 2nd part BridgeLabz
+	public void EmailValidationItShouldReturnValidOrNotaddsymbolat() {
+		EmailValidation email = new EmailValidation();
+		assertTrue(email.validemail("abc.xyz@BridgeLabz")); // validated True
+		assertTrue(email.validemail("Anand.256@BridgeLabz"));// validated True
+		// test case passed
+	}
+
 }
