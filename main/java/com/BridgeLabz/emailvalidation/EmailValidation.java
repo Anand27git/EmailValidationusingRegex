a6f2808691e90a93ec3ccefe3a6024ce92cc9e32
Anand27git <anandkumar256k@gmail.com>
2021-06-19 08:35:03 +0530

    validating 3rd mandatory part

--- main/java/com/BridgeLabz/emailvalidation/EmailValidation.java
+++ main/java/com/BridgeLabz/emailvalidation/EmailValidation.java
@@ -1,3 +1,4 @@
+
 package com.BridgeLabz.emailvalidation;
 
 import java.util.regex.Pattern;
@@ -5,15 +6,18 @@ import java.util.regex.Pattern;
 /********************
  * 
  * @author ANAND 
+ * 
  * purpose: Validation of Email-ID
  * UC-1 Validation of Email-ID start with abc
  * UC-2 to Ensure the @ and mandatory 2nd part BridgeLabz
+ * UC-3 to Ensure the dot and mandatory 3rd part after BridgeLabz
+ * 
  ********************/
 public class EmailValidation {
 
 	// UC-2 to Ensure the @ and mandatory 2nd part BridgeLabz
 	public boolean validemail(String email) {
-		return Pattern.matches("^[a-zA-Z0-9.@a-zA-Z0-9.]+$", email);
+		return Pattern.matches("^([a-zA-Z0-9.])+([0-9a-zA-Z]{0,1}+@([a-zA-Z0-9.])+[a-zA-Z]{2,3})$", email);
 	}
 
 }
--- test/java/com/BridgeLabz/emailvalidation/EmailValidationtest.java
+++ test/java/com/BridgeLabz/emailvalidation/EmailValidationtest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
  * @author ANAND 
  * UC-1 Test case to validate mandatory part start with abc
  * UC-2 Test case to Ensure the @ and mandatory 2nd part BridgeLabz
+ * UC-3 Test case to Ensure the dot and mandatory 3rd part after BridgeLabz
  * 
  *******************/
 public class EmailValidationtest {
@@ -30,5 +31,12 @@ public class EmailValidationtest {
 		assertTrue(email.validemail("Anand.256@BridgeLabz"));// validated True
 		// test case passed
 	}
-
+	@Test
+	// UC-3 Test case to Ensure the dot and mandatory 3rd part after BridgeLabz
+	public void EmailValidationItShouldReturnValidOrNotaddDotSymbol() {
+		EmailValidation email = new EmailValidation();
+		assertTrue(email.validemail("abc.xyz@BridgeLabz.in")); // validated True
+		assertTrue(email.validemail("Anand.256@BridgeLabz"));// validated True
+		// test case passed
+	}
 }
