25dcf2a1fbaa783c6b6cd0f0e05246aef927d73d
Anand27git <anandkumar256k@gmail.com>
2021-06-19 08:45:08 +0530

    validateing optionalpart

--- main/java/com/BridgeLabz/emailvalidation/EmailValidation.java
+++ main/java/com/BridgeLabz/emailvalidation/EmailValidation.java
@@ -11,13 +11,14 @@ import java.util.regex.Pattern;
  * UC-1 Validation of Email-ID start with abc
  * UC-2 to Ensure the @ and mandatory 2nd part BridgeLabz
  * UC-3 to Ensure the dot and mandatory 3rd part after BridgeLabz
+ * UC-4 to Ensure the optional part
  * 
  ********************/
 public class EmailValidation {
 
-	// UC-2 to Ensure the @ and mandatory 2nd part BridgeLabz
+	// UC-4 to Ensure the optional part
 	public boolean validemail(String email) {
-		return Pattern.matches("^([a-zA-Z0-9.])+([0-9a-zA-Z]{0,1}+@([a-zA-Z0-9.])+[a-zA-Z]{2,3})$", email);
+		return Pattern.matches("^([a-zA-Z0-9.])+([0-9a-zA-Z]{0,1}+@([a-zA-Z0-9.])+[a-zA-Z]{2,3})+([a-z]{0,2}){0,1}$", email);
 	}
 
 }
--- test/java/com/BridgeLabz/emailvalidation/EmailValidationtest.java
+++ test/java/com/BridgeLabz/emailvalidation/EmailValidationtest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
  * UC-1 Test case to validate mandatory part start with abc
  * UC-2 Test case to Ensure the @ and mandatory 2nd part BridgeLabz
  * UC-3 Test case to Ensure the dot and mandatory 3rd part after BridgeLabz
+ * UC-4 Test case to Ensure the optional part
  * 
  *******************/
 public class EmailValidationtest {
@@ -37,6 +38,15 @@ public class EmailValidationtest {
 		EmailValidation email = new EmailValidation();
 		assertTrue(email.validemail("abc.xyz@BridgeLabz.in")); // validated True
 		assertTrue(email.validemail("Anand.256@BridgeLabz"));// validated True
+		// test case passed
+	}
+	
+	@Test
+	// UC-4 Test case to Ensure the optional part
+	public void EmailValidationItShouldReturnValidOrNotaddoptionalpart() {
+		EmailValidation email = new EmailValidation();
+		assertTrue(email.validemail("abc.xyz@BridgeLabz.org.in")); // validated True
+		assertTrue(email.validemail("Anand.256@BridgeLabz.com.in"));// validated True
 		// test case passed
 	}
 }
